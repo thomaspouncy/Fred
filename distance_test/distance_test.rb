fd7422c74063db5306e952e255cd72f998ca7e00
thomaspouncy <thomas.pouncy@gmail.com>
2012-02-15 22:24:01 -0800

    added event based memory

--- distance_test/distance_test.rb
+++ distance_test/distance_test.rb
@@ -14,6 +14,50 @@ def setup_mongo
   @col = @db.collection("distance_memory")
 end
 
+def store_memory_in_long_term
+  @col.insert({:_id=>Time.now.to_s,:memory=>@memory_queue})
+end
+
+def start_memory_event(event_name)
+  raise "Already processing event #{@current_event[:_id]}" unless @current_event.nil?
+  raise "Invalid event name" if event_name.nil?
+  raise "No collection set" if @col.nil?
+
+  @current_event = @col.find_one({:_id => event_name})
+  if @current_event.nil?
+    @col.insert({:_id=>event_name})
+    @current_event = {:_id=>event_name}
+  end
+end
+
+def end_memory_event(event_name)
+  @col.update({:_id=>event_name},@current_event)
+  @current_event = nil
+end
+
+def start_memory_event_section(section_name)
+  raise "Already processing event section #{@current_event_section[:_id]}" unless @current_event_section.nil?
+  raise "Invalid section name" if section_name.nil?
+  raise "No current event set" if @current_event.nil?
+
+  @current_section = @current_event[section_name.to_sym] || []
+  @current_section << {:_id => Time.now.to_s, :info => {} }
+  @current_section
+end
+
+def end_memory_event_section(section_name)
+  @current_event[section_name.to_sym] = @current_section
+  @current_section = nil
+end
+
+def remember_event_information(info_hash)
+  @current_event = @current_event.merge(info_hash)
+end
+
+def remember_event_section_information(info_hash)
+  @current_section.last[:info] = @current_section.last[:info].merge(info_hash)
+end
+
 def get_distance
   @memory_queue << [@us.distance,Time.now.to_f]
   puts "Distance: #{@memory_queue.last[0]}in"
@@ -53,21 +97,23 @@ def motors_reach_top_speed
   ((@memory_queue[-1][0] - @memory_queue[-2][0]).abs - (@memory_queue[-2][0] - @memory_queue[-3][0]).abs) <= TOP_SPEED_THRESHOLD ? true : false
 end
 
-def store_memory_in_long_term
-  @col.insert({:_id=>Time.now.to_s,:memory=>@memory_queue})
-end
-
-def forget_everything
-  store_memory_in_long_term
+def clear_short_term_memory
   @memory_queue.clear
 end
 
 def calculate_friction
+  start_memory_event("friction_test")
+
   distances = []
   power_levels = [50,75,100]
   invalid_power_levels = []
+  ratios = []
+
   power_levels.each do |power_level|
-    forget_everything
+    start_memory_event_section("power_level_#{power_level}")
+
+    clear_short_term_memory
+
     start_motors(power_level)
     until motors_reach_top_speed
       sleep(SLEEP_INTERVAL)
@@ -83,17 +129,28 @@ def calculate_friction
     if stop_distance == start_distance
       puts "Power level #{power_level} was not high enough to overcome friction on this surface"
       invalid_power_levels << power_level
+
+      remember_event_information({:invalid_power_levels => invalid_power_levels})
     else
       distances << (stop_distance - start_distance).abs
+      ratios << distances.last.to_f/power_level
+
+      remember_event_section_information({:distance=>distances.last,:ratio=>ratios.last})
     end
+
+    end_memory_event_section("power_level_#{power_level}")
   end
 
   puts "distances were: #{distances.inspect}"
 
-  ratios = []
-  (power_levels-invalid_power_levels).each_index {|x| ratios[x] = distances[x].to_f/power_levels[x] }
   puts "ratios are: #{ratios.inspect}"
   estimated_friction = ratios.inject(0) {|sum,ratio| sum+=ratio }.to_f/ratios.length
+
+  remember_event_information({:estimated_friction => estimated_friction })
+
+  end_memory_event("friction_test")
+
+  estimated_friction
 end
 
 def drive_to_nearest_wall
@@ -168,4 +225,4 @@ $DEBUG = false
 # drive_to_nearest_wall
 puts "Estimated friction was: #{calculate_friction}"
 
-forget_everything
+clear_short_term_memory
